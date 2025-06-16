44210b54713cbd17a73f0f093592abe47231d5bc
Edwin Cardenas <ecardenas@lwolf.com>
2025-06-16 07:13:45 -0500

    feat: fetch and process image data

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -5,7 +5,12 @@
 //  Created by Edwin Cardenas on 6/14/25.
 //
 
-import Foundation
+import UIKit
+
+enum PhotoError: Error {
+    case imageCreationError
+    case missingImageURL
+}
 
 class PhotoStore {
 
@@ -37,5 +42,42 @@ class PhotoStore {
         }
 
         return FlickrAPI.photos(fromJson: jsonData)
+    }
+
+    func fetchImage(
+        for photo: Photo,
+        completion: @escaping (Result<UIImage, Error>) -> Void
+    ) {
+        guard let photoURL = photo.remoteURL else {
+            completion(.failure(PhotoError.missingImageURL))
+
+            return
+        }
+
+        let request = URLRequest(url: photoURL)
+
+        let task = session.dataTask(with: request) { data, response, error in
+            let result = self.processImageRequest(data: data, error: error)
+
+            completion(result)
+        }
+
+        task.resume()
+    }
+
+    private func processImageRequest(data: Data?, error: Error?) -> Result<
+        UIImage, Error
+    > {
+        guard let imageData = data,
+            let image = UIImage(data: imageData)
+        else {
+            if data == nil {
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+
+        return .success(image)
     }
 }
