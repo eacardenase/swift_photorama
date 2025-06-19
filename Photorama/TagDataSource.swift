2f60c31baed9c9b5d3ebd361f08c029522860b8d
Edwin Cardenas <ecardenas@lwolf.com>
2025-06-19 15:58:59 -0500

    feat: add tags data source

---
+++ Photorama/TagDataSource.swift
@@ -0,0 +1,38 @@
+//
+//  TagDataSource.swift
+//  Photorama
+//
+//  Created by Edwin Cardenas on 6/19/25.
+//
+
+import CoreData
+import UIKit
+
+class TagDataSource: NSObject, UITableViewDataSource {
+
+    var tags = [Tag]()
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int
+    {
+        return tags.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: NSStringFromClass(UITableViewCell.self),
+            for: indexPath
+        )
+
+        let tag = tags[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+
+        content.text = tag.name
+
+        cell.contentConfiguration = content
+
+        return cell
+    }
+}
